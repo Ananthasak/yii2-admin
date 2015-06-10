891ce9ec3b2a041b7e0e3fada026f89e5e30593a
Ananthasak <von_99@windowslive.com>
2015-06-10 09:52:14 +0700

    Create rbac-admin.php

---
+++ messages/lo/rbac-admin.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Message translations for \mdmsoft\yii2-admin.
+ *
+ * It contains the localizable messages extracted from source code.
+ * You may modify this file by translating the extracted messages.
+ *
+ * Each array element represents the translation (value) of a message (key).
+ * If the value is empty, the message is considered as not translated.
+ * Messages that no longer need translation will have their translations
+ * enclosed between a pair of '@@' marks.
+ *
+ * Message string can be used with plural forms format. Check i18n section
+ * of the guide for details.
+ *
+ * NOTE: this file must be saved in UTF-8 encoding.
+ */
+return [
+    'Assignments' => 'ກຳໜົດຄ່າ',
+    'Users' => 'ຜູ້ໃຊ້ງານ',
+    'User' => 'ຜູ້ໃຊ້ງານ',
+    'Avaliable' => 'ບ່ອນຢູ່',
+    'Assigned' => 'ທີ່ກຳໜົດໃຫ້',
+    'Create' => 'ສ້າງ',
+    'Update' => 'ປັບປຸງ',
+    'Roles' => 'ບົດບາດ',
+    'Create Role' => 'ສ້າງບົດບາດ',
+    'Name' => 'ຊື່',
+    'Type' => 'ຊະນິດ',
+    'Description' => 'ລາຍລະອຽດ',
+    'Rule Name' => 'ຊື່ກົດ',
+    'Data' => 'ຂໍ້ມູນ',
+    'Update Role' => 'ປັບປຸງ',
+    'Delete' => 'ລືບ',
+    'Are you sure to delete this item?' => 'ທ່ານໝັ້ນໃຈບໍ່ວ່າຕ້ອງການລືບອັນນີ້?',
+    'ID' => 'ໄອດີ',
+    'Parent' => 'ຕົວຫຼັກ',
+    'Parent Name' => 'ຊື່ ຕົວຫຼັກ',
+    'Route' => 'ເສັ້ນທ່າງ',
+    'Username' => 'ຊື່ຜູ້ໃຊ້',
+    'Update Permission' => 'ປັບປຸງສິດ',
+    'Permissions' => 'ສິດ',
+    'Permission' => 'ສິດ',
+    'Create Permission' => 'ສ້າງສິດ',
+    'Create Permissions' => 'ສ້າງສິດ',
+    'Routes' => 'ເສັ້ນທາງ',
+    'Create route' => 'ສ້າງເສັ້ນທ່າ',
+    'New' => 'ໃໝ່',
+    'Generate Routes' => 'ເສັ້ນທາງອັດຕະໂນມັດ',
+    'Append' => 'ຜະນວກ',
+    'Create Rule' => 'ສ້າງກົດ',
+    'Rules' => 'ກົດ',
+    'Update Rule' => 'ປັບປຸງກົດ',
+    'Create Menu' => 'ສ້າງເມນູ',
+    'Menus' => 'ເມນູ',
+    'Search' => 'ຄົ້ນຫາ',
+    'Reset' => 'ເລີ້ມໃໝ່',
+    'Update Menu' => 'ປັບປຸງເມນູ',
+    'Menus' => 'ເມນູ',
+    'Order' => 'ຮຽງລຳດັບ',
+    'Class Name' => 'ຊື່ຄາດ',
+    'Assignment' => 'ການກຳໜົດ',
+    'Role' => 'ບົບບາດ',
+    'Rule' => 'ກົດ',
+    'Menu' => 'ເມນູ',
+    'Help' => 'ຊ່ວຍເຫຼືອ',
+    'Application' => 'ແອບພິເຄຊັ້ນ',
+];
